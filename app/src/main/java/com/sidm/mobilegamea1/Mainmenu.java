a815b1af83105dfcde2e1e33373d7c0a38c8c5c9
Princeton Chew <PrincetonCYK@hotmail.com>
2015-11-23 19:49:54 +0900

    first push

---
+++ app/src/main/java/com/sidm/mobilegamea1/Mainmenu.java
@@ -0,0 +1,60 @@
+package com.sidm.mobilegamea1;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+
+public class Mainmenu extends Activity implements OnClickListener{
+
+    private Button btn_start;
+    private Button btn_help;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);// hide title
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN); //hide top bar
+
+        setContentView(R.layout.mainmenu);
+
+        btn_start = (Button)findViewById(R.id.btn_start);
+        btn_start.setOnClickListener(this);
+
+        btn_help = (Button)findViewById(R.id.btn_help);
+        btn_help.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        Intent intent = new Intent();
+
+        if(v == btn_start)
+        {
+            intent.setClass(this,Splashpage.class);
+        }
+        else if( v == btn_help)
+        {
+            //intent.setClass(this,Helppage.class);
+        }
+
+        startActivity(intent);
+    }
+
+    protected void onPause(){
+        super.onPause();
+    }
+
+    protected void onStop(){
+        super.onStop();
+    }
+
+    protected void onDestroy(){
+        super.onDestroy();
+    }
+}
